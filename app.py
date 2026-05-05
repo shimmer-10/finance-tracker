b5d0e3dc5b4747a769d1cfc494a7a398b223b185
Anshika <your-email@example.com>
2026-05-05 13:28:09 +0530

    Updated budget logic

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,redirect,url_for
 
 app=Flask(__name__)
 expenses=[]
 @app.route("/", methods=["GET","POST"])
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html",expense=expenses)
 
 @app.route("/expense" , methods=["GET","POST"])
 def expense():
@@ -12,8 +12,8 @@ def expense():
 
 @app.route("/add", methods=["GET","POST"])
 def add():
+    source=request.args.get("source")
     if request.method=="POST":
-        source=request.args.get("source")
         expense_name=request.form.get("expense_name")
         category=request.form.get("category")
         amount=request.form.get("amount")
@@ -21,8 +21,13 @@ def add():
 
         new_expense={"expense_name" : expense_name , "category":category , "amount":amount, "date": date}
         expenses.append(new_expense)
+        
+        if source=="expense":
+            return redirect(url_for("expense"))
+        else:
+            return redirect(url_for("dashboard"))
 
-    return render_template("add.html")
+    return render_template("add.html", source=source)
 
 if __name__=="__main__":
     app.run(debug=True)
